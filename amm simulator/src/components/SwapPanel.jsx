0230c77a2f7c3a363b2fa21f966a9d29dcb5126c
Naveen pandian <npandian515@gmail.com>
2025-08-19 10:39:05 +0530

    logic for eth to usdc

--- amm simulator/src/components/SwapPanel.jsx
+++ amm simulator/src/components/SwapPanel.jsx
@@ -6,7 +6,25 @@ const SwapPanel = ({}) => {
     const fee = 0.03;
 
     const swap =() => {
-        
+        let {eth , usdc} = ResizeObserverSize;
+
+        const input = parseFloat(amount);
+
+        if(!input) return;
+
+        if(direction === "ethToUsdc") {
+            const inputWithFee = input * (1- fee);
+
+           const output = (usdc * inputWithFee ) / (eth + inputWithFee);
+            
+           setReserves({eth: eth + input , usdc: usdc - input});
+
+           logEvent(`Swapped ${input} USDC for ${output.toFixed(4)} ETH  `)
+        }  
+
+
+
+
     }
 
   return (
