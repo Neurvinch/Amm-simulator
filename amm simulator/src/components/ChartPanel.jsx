12af2350b2a87a279b77b2339a5255d9e48efe6e
Naveen pandian <npandian515@gmail.com>
2025-08-19 13:28:51 +0530

    chard pannel is played into our sim ammm

---
+++ amm simulator/src/components/ChartPanel.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ChartPanel = ({}) => {
+
+    const data = history.map((h)=> ({
+        time: new Date(h.time).toLocaleTimeString(),
+        price: h.usdc / h.eth
+    }))
+
+  return (
+     <div className="bg-gray-800 p-4 rounded-xl shadow-lg">
+      <h2 className="text-xl font-bold mb-2">Price History</h2>
+      <ResponsiveContainer width="100%" height={200}>
+        <LineChart data={data}>
+          <XAxis dataKey="time" hide />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="price" stroke="#3b82f6" dot={false} />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+export default ChartPanel
